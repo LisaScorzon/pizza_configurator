a1ad756d403789cffcd42aa2d797824f8c73a0de
LisaScorzon <lisabailey.eu@gmail.com>
2018-04-28 21:49:38 +0200

    added seperate ingredients folder

---
+++ src/ingredients/ingredients.js
@@ -0,0 +1,103 @@
+const bases = [
+    {
+    id:1,
+    size: '25cm NY Style',
+    price: '8.99',
+    },
+    
+    {
+      id:2,
+    size: '30cm NY Style',
+    price: '11.49',
+  
+  
+    }, 
+    
+    {
+      id:3,
+    size: '35cm NY Style',
+    price: '13.49',
+  
+    },
+    {
+  
+      id:4,
+    size: '20cm NY Style',
+    price: '6.45',
+    }
+  ]
+
+  export const sauces = [
+    {
+    id:1,
+    name: 'White sauce',
+    price: '0 (included in price)',
+    },
+    
+    {
+      id:2,
+    name: 'Red sauce',
+    price: '0 (included in price)',
+  
+  
+    }, 
+    
+    {
+      id:3,
+    name: 'Double red sauce',
+    price: '1.50',
+  
+    },
+    {
+  
+      id:4,
+    name: 'Mix it up',
+    price: '0.50',
+    }
+  ]
+
+  const toppings = [
+    {
+    id:1,
+    name: 'Pineapple',
+    price: '0.50',
+    },
+    
+    {
+      id:2,
+    name: 'Corn',
+    price: '0.50',
+    }, 
+    
+    {
+      id:3,
+    name: 'Olives (green)',
+    price: '0.50',
+  
+    },
+    {
+  
+      id:4,
+    name: 'Red Onion',
+    price: '0.50',
+    },
+    {
+        id:5,
+      name: 'Spinach',
+      price: '0.50',
+    
+      },
+      {
+        id:6,
+      name: 'Cherry tomatoes',
+      price: '0.50',
+    
+      },
+      {
+        id:7,
+      name: 'Chicken',
+      price: '0.50',
+    
+      }
+
+  ]
